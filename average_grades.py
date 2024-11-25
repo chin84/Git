2412c04bc0575b1be9c61e6fbedfd32c3f70faca
chin84 <chin84@inbox.ru>
2024-11-25 17:30:39 +0300

    Update average_grades.py

--- average_grades.py
+++ average_grades.py
@@ -1,7 +1,6 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 lst_students = sorted(list(students))
-print(lst_students)
 dict_students_and_grades = {}
 dict_students_and_grades.update({lst_students[0]: sum(grades[2])/len(grades[2]),
                                 lst_students[1]: sum(grades[1])/len(grades[1]),
